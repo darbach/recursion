c9a2595fc8a043d9ea9169698e4afc6e0bade6c6
darbach <danielarbach@gmail.com>
2021-03-09 10:38:02 -0700

    Use @CsvFileSource for tests.

--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -3,43 +3,24 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class FactorialsTest {
 
-  static final long[][] testCases = {
-      {0, 1},
-      {1, 1},
-      {5, 120},
-      {10, 3628800},
-      {13, 6227020800L}
-  };
-
   @DisplayName("computeRecursive(non-exceptions)")
-  @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) == {1}")
-  @MethodSource
+  @ParameterizedTest(name = "[{index}] Asserting computeRecursive({0}) == {1}.")
+  @CsvFileSource(resources = "factorials-test-non-exception.csv", numLinesToSkip = 1)
   void computeRecursive_nonException(int input, long expected) {
     assertEquals(expected, Factorials.computeRecursive(input));
   }
 
   @DisplayName("computerRecursive(exceptions)")
-  @Test
-  void computeRecursive_exception() {
-    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
+  @ParameterizedTest(name = "[{index}] Asserting computeRecursive_exception({0}) throws IllegalArgumentException.")
+  @CsvFileSource(resources = "factorials-test-exception.csv", numLinesToSkip = 1)
+  void computeRecursive_exception(int input) {
+    assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(input));
   }
 
-  static List<Arguments> computeRecursive_nonException() {
-    return List.of(
-        Arguments.of(0, 1L),
-        Arguments.of(1, 1L),
-        Arguments.of(5, 120L),
-        Arguments.of(10, 3_628_800L),
-        Arguments.of(13, 6_227_020_800L)
-    );
-  }
 }
